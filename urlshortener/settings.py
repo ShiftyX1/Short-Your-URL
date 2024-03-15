05e8524186c418f56a5677881984c6d48ce7419f
ShiftyX1 <143071695+ShiftyX1@users.noreply.github.com>
2024-03-15 11:50:33 +0300

    small changes

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -77,10 +77,24 @@ WSGI_APPLICATION = 'urlshortener.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shorturl_db',
+        'USER': 'django_admin',
+        'PASSWORD': 'Temp1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
