6754fd2cbde0949138fc5837c729c7d55ddb7d52
ShiftyX1 <143071695+ShiftyX1@users.noreply.github.com>
2024-03-15 12:37:09 +0300

    docker-compose added, edited settings.py

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -75,14 +75,26 @@ WSGI_APPLICATION = 'urlshortener.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'shorturl_db',
+#        'USER': 'django_admin',
+#        'PASSWORD': 'Temp1234',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shorturl_db',
-        'USER': 'django_admin',
-        'PASSWORD': 'Temp1234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'shorturl_db',
+        'PORT': 5432,
     }
 }
 
